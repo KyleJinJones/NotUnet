922cd285da50c283370e58456c0f6375657c2273
unknown <malbayat@uci.edu>
2019-05-15 17:39:42 -0700

    Enemies Spawn in random points with interval set

--- FantasyShooter/Assets/Scripts/WaveManager.cs
+++ FantasyShooter/Assets/Scripts/WaveManager.cs
@@ -4,37 +4,47 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    [SerializeField] private GameObject enemy; // Enemy prefab
     [SerializeField] private int enemiesLeft;
     [SerializeField] private int enemiesToSpawn;
     [SerializeField] private float spawnRate;   // per seconds
 
     private Vector3[] spawnPoints;
-    private float timer;
+    [SerializeField] private float timer;
+
+    private bool stopSpawning;
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = spawnRate; // conver to seconds
+        timer = spawnRate; 
+        stopSpawning = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        timer -= Time.deltaTime;
+        if(!stopSpawning) {timer -= Time.deltaTime;}
 
-        if(timer <= 0)
+        if(timer <= 0 )
         {
             initSpawnPoints(enemiesToSpawn);
+            Debug.Log("b");
             spawnEnemies(enemiesToSpawn);
+
+            enemiesLeft -= enemiesToSpawn;
+            timer = spawnRate;
         }
+
+        if(enemiesLeft == 0) {stopSpawning = true;}
     }
 
     public void spawnEnemies(int numOfEnemies)
     {
-        for (int i = numOfEnemies; i > 0; --i)
+        for (int i = numOfEnemies - 1; i >= 0; --i)
         {
-            // enemy instantiation here
+            Instantiate(enemy, spawnPoints[i], Quaternion.identity);
         }
     }
 
@@ -42,12 +52,9 @@ public class WaveManager : MonoBehaviour
     {
         spawnPoints = new Vector3[length];
 
-        //random generation goes here
-
-
         for(int i = 0; i < length; ++i)
         {
-            Vector3 randomPoint = new Vector3(0, 0, 0);
+            Vector3 randomPoint = new Vector3(Random.Range(-5, 5), 1.3f, Random.Range(-5, 5));
             spawnPoints[i] = randomPoint;
         }
 
