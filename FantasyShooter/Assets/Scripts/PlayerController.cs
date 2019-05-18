a8352aad955c354f5de4fc74f5680389bfcb489f
unknown <malbayat@uci.edu>
2019-05-18 12:16:38 -0700

    Importing assets

---
+++ FantasyShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Car")) {
+            if(Input.GetKeyDown(KeyCode.E)) {
+                
+            }
+        }
+
+
+    }
+}
