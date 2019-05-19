88dda843712338f4b60dbcb2a6a858c82415fcec
Kyle Jin Jones <kjjones1@uci.edu>
2019-05-18 20:43:28 -0700

    Basic Movement tracking

---
+++ FantasyShooter/Assets/Scripts/MonsterMov.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMov : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject player;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            Debug.Log("PLAYER NOT SET");
+        }
+        else
+        {
+            Vector3 dir = player.transform.position- transform.position;
+            
+            Quaternion rot = Quaternion.LookRotation(dir);
+            transform.rotation = rot;
+            dir.Normalize();
+            dir *= speed;
+            rb.velocity = dir;
+            
+        }
+    }
+}
