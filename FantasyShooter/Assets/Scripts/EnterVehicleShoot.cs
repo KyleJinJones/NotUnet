77d0f7b95e8240750e71c1cb82fc388316209f8c
unknown <malbayat@uci.edu>
2019-05-27 13:02:36 -0700

    Still needs fixing

--- FantasyShooter/Assets/Scripts/EnterVehicleShoot.cs
+++ FantasyShooter/Assets/Scripts/EnterVehicleShoot.cs
@@ -7,10 +7,15 @@ public class EnterVehicleShoot : MonoBehaviour
 {
     private bool inVehicle = false;
     [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject seat;
+
+    [SerializeField] private vp_Controller m_Component;
     
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")) {
             player = other.gameObject;
+            m_Component = player.GetComponent<vp_Controller>();
         }
     }
 
@@ -21,18 +26,21 @@ public class EnterVehicleShoot : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.G))
             {
-                inVehicle = true; 
-                player.transform.Translate(new Vector3(0f, 2f, 2f));          
-                player.transform.parent = gameObject.transform;       
+                inVehicle = true;    
+                player.transform.parent = seat.transform;
+                player.transform.position = seat.transform.position;     
+                // m_Component.PhysicsGravityModifier = 0f;
             }
         }  
     }
     void Update()
     {
+
         if (inVehicle == true && Input.GetKey(KeyCode.F))
         {
             inVehicle = false;
-            player.transform.parent = null;       
+            player.transform.parent = null;    
+            // m_Component.PhysicsGravityModifier = 0.2f;
         }
     }
 }
