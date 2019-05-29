3bafff1fe41454cbaa9e4809f33faa6f238f1827
Kyle Jin Jones <kjjones1@uci.edu>
2019-05-29 01:40:10 -0700

    added fx, and simple magic

---
+++ FantasyShooter/Assets/Scripts/Player/FireMagic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMagic : MonoBehaviour
+{
+    public GameObject effect;
+    public float timer;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+           GameObject temp = Instantiate(effect, this.transform.position, this.transform.rotation);
+            temp.transform.Translate(.5f, 0, 0);
+            
+        }
+    }
+}
