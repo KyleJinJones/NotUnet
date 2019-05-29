95b42fe171c63ee3196cf6e4770a0c45aeb085b7
Kyle Jin Jones <kjjones1@uci.edu>
2019-05-28 18:16:54 -0700

    pistol pulled out on start

---
+++ FantasyShooter/Assets/Scripts/Testscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponent<vp_FPInput>().FPPlayer.SetWeapon.TryStart(1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
