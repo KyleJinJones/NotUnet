4458d00fb436b24721b975b93bf0d33e7a70e5f4
Kyle Jin Jones <kjjones1@uci.edu>
2019-06-06 13:39:12 -0700

    added Cooldown to magic

--- FantasyShooter/Assets/Scripts/Player/FireMagic.cs
+++ FantasyShooter/Assets/Scripts/Player/FireMagic.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class FireMagic : MonoBehaviour
 {
     public GameObject effect;
-    public float timer;
+    public float timer= 1.0f;
+    private float basetime;
+    public Image i;
+    public TextMeshProUGUI timertext;
    
     void Start()
     {
-        
+        basetime = timer;
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse2))
+        if (timer < basetime)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > basetime)
+            {
+                timer = basetime;
+            }
+            i.fillAmount = timer/basetime;
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Mouse2)&&timer>=basetime)
         {
            GameObject temp = Instantiate(effect, this.transform.position, this.transform.rotation);
             temp.transform.Translate(.5f, 0, 0);
+            timer = 0;
             
         }
+        timertext.text = string.Format("{0:0.00}", timer);
     }
 }
