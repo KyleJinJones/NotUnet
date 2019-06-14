576f0771a0d284de8c334f232bc082e76a2d37cd
seantsukim <seantsukim@gmail.com>
2019-06-13 22:09:41 -0700

    added quit functionality
    added quit functionality into the game when player presses esc, havent tested on controller yet. SHould be functional and good to go.

---
+++ FantasyShooter/Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///This script will be attached to a gameobject to hold all the functionality that a button needs when paused.
+public class ButtonFunction : MonoBehaviour
+{
+    [SerializeField]private GameObject parentUI;
+    //set the time scale to 0 to stop all objects from moving in the scene
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    //set the time scale to 1 to allow all objects to move in the scene
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+
+    //quit the game completely, back the the Desktop
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ FantasyShooter/Assets/Scripts/PauseUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    [SerializeField]private GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pauseMenu == null)
+            Debug.Log("Put in the Pause Menu for this to WORK!");
+        else
+            pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
+        {
+            //we use time scale to check if the menu should show in the game
+            //if (Time.timeScale == 1)
+            //    Time.timeScale = 0;
+            //else
+            //    Time.timeScale = 1;
+            Debug.Log("Unity Close");
+            Application.Quit();
+        }
+        //reveal mouse and menu when screen is paused (time scale is 0)
+        //Cursor.visible = (Time.timeScale == 0);
+        //pauseMenu.SetActive((Time.timeScale == 0));
+
+        //Debug.Log("THE CURRENT TIME SCALE IS: " + Time.timeScale.ToString());    
+    }
+}
